b24e0c57828d0d6e8fa4c2724cbae4e7d2ef8b3f
vertigoaway <connormm2@protonmail.com>
2026-02-24 16:11:33 -0500

    Genericify and add tok dict

--- charTokenizer.py
+++ charTokenizer.py
@@ -2,14 +2,12 @@ import csv
 import numpy as np
 import numpy.typing as npt
 import time
-
+from typing import Any
 ## lazily tokenizes all characters from an exported discord chat csv ##
 # message size is static, most uses for this are for AI/ML
 
 # TODO:
     # 1. detect and remove links inside of contents (L:44-45)
-    # 2. remove usage of unicode and implement a better system (L:59-64)
-        # prob a fat dictionary
     # 3. userid blacklist (OPTIONAL)
     # 4. add unique userids to msgs (OPTIONAL)
     # 5. add multithreading (OPTIONAL)
@@ -18,6 +16,8 @@ import time
 # accepted format:
     # "AuthorID","Author","Date","Content","Attachments","Reactions"
 #only uses the content and attachments :p
+
+
 
 def charTokenize(
         readOut: list[list[str]],
@@ -44,6 +44,7 @@ def charTokenize(
     link: str
     msgarr: list[str]
     entries: npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32] 
+    tokDict : dict[str,int] = {'a':4,'b':5,'c':6,'d':7,'e':8,'f':9,'g':10,'h':11,'i':12,'j':13,'k':14,'l':15,'m':16,'n':17,'o':18,'p':19,'q':20,'r':21,'s':22,'t':23,'u':24,'v':25,'w':26,'x':27,'y':28,'z':29,' ':30,'.':31,',':32,'\'':33,'/':34,'\"':35,':':36,';':37,'1':38,'2':39,'3':40,'4':41,'5':42,'6':43,'7':44,'8':45,'9':46,'0':47}
 
     ct: int = 0
     x: float = time.time()
@@ -54,10 +55,7 @@ def charTokenize(
     ct = 0 #optional, used to calc tok/s
     for row in readOut:
         msg = row[3]
-        link = row[4]
 
-        if link != "":  # theres a link in the msg!!!
-            msg += chr(lnkTok) 
         try:
             if msg[0:1] == ["> "]:  # checks if message is a reply
                 msg = msg.split("\n", 1)[-1]  # remove quoted text, leave response
@@ -72,10 +70,11 @@ def charTokenize(
 
         msgarr = list(msg)
         for id, letter in enumerate(msgarr):
-            if ord(letter) > MAX: #too big for current dtype :(
+            try:
+                entries[ct][id] = tokDict[letter]
+            except:
                 entries[ct][id] = nulTok
                 continue
-            entries[ct][id] = ord(letter)
         entries[ct][-1] = eosTok 
 
 
@@ -83,7 +82,6 @@ def charTokenize(
     print(f"time taken: {time.time()-x}s")
     print(f"token speed: {(msgSize)*ct/(time.time()-x)}/s")
     return entries
-
 
 
 
