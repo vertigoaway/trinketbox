bdac15cd9c28956083057ceb2382f9b0d07b76cf
vertigoaway <connormm2@protonmail.com>
2026-02-25 09:52:39 -0500

    Refactoring

--- charTokenizer.py
+++ charTokenizer.py
@@ -8,9 +8,12 @@ from typing import Any
 
 # TODO:
     # 1. detect and remove links inside of contents (L:44-45)
-    # 3. userid blacklist (OPTIONAL)
-    # 4. add unique userids to msgs (OPTIONAL)
-    # 5. add multithreading (OPTIONAL)
+    # 2. genericify discordStaticToken and utilize __tokenizeLine
+    # 3. make tokDict a parameter
+    # 4. userid blacklist (OPTIONAL)
+    # 5. add unique userids to msgs (OPTIONAL)
+    # 6. add multithreading (OPTIONAL)
+    # 7. make pylance SHUT THE FUCK UP!!!!
 
 
 # accepted format:
@@ -19,16 +22,17 @@ from typing import Any
 
 
 
-def charTokenize(
+def discordStaticTokenize( 
         readOut: list[list[str]],
         msgSize: int = 256,
         nulTok: int = 0,
         eosTok: int = 1,
         lnkTok: int = 2,
-        dType = np.uint32 
+        dType = np.uint32,
+        static: bool = True 
         ) -> npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32]:# if you go any higher fuck off
     """
-    Parses variables from an environment file.
+    Tokenizes all messages from a discord chat export with a static size
 
     :param readOut: Contents of a CSV discord chat export.
     :param msgSize: Size of all messages.
@@ -47,7 +51,6 @@ def charTokenize(
     tokDict : dict[str,int] = {'a':4,'b':5,'c':6,'d':7,'e':8,'f':9,'g':10,'h':11,'i':12,'j':13,'k':14,'l':15,'m':16,'n':17,'o':18,'p':19,'q':20,'r':21,'s':22,'t':23,'u':24,'v':25,'w':26,'x':27,'y':28,'z':29,' ':30,'.':31,',':32,'\'':33,'/':34,'\"':35,':':36,';':37,'1':38,'2':39,'3':40,'4':41,'5':42,'6':43,'7':44,'8':45,'9':46,'0':47}
 
     ct: int = 0
-    x: float = time.time()
 
     for row in readOut:
         ct += 1 # count rows to statically allocate numpy array
@@ -65,7 +68,6 @@ def charTokenize(
             msg = msg[0 : msgSize - 1]  # get rid of text above size
         except:
             pass
-
         msg += "" * (msgSize - 1 - len(msg)) #fatten up to size
 
         msgarr = list(msg)
@@ -79,15 +81,60 @@ def charTokenize(
 
 
         ct += 1
-    print(f"time taken: {time.time()-x}s")
-    print(f"token speed: {(msgSize)*ct/(time.time()-x)}/s")
     return entries
+
+def __tokenizeLine( #tokenizes a single line of any size, makes life easier
+        msg: str,
+        nulTok: int = 0,
+        eosTok: int = 1,
+        lnkTok: int = 2,
+        dType = np.uint32,
+        ) -> npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32]:# if you go any higher fuck off
+    MAX : int = np.iinfo(dType).max
+    link: str
+    msgarr: list[str]
+    entry: npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32] 
+    tokDict : dict[str,int] = {'a':4,'b':5,'c':6,'d':7,'e':8,'f':9,'g':10,'h':11,'i':12,'j':13,'k':14,'l':15,'m':16,'n':17,'o':18,'p':19,'q':20,'r':21,'s':22,'t':23,'u':24,'v':25,'w':26,'x':27,'y':28,'z':29,' ':30,'.':31,',':32,'\'':33,'/':34,'\"':35,':':36,';':37,'1':38,'2':39,'3':40,'4':41,'5':42,'6':43,'7':44,'8':45,'9':46,'0':47}
+
+    entry = np.zeros(len(msg)+1,dtype=dType)
+    msgarr = list(msg)
+
+    for id,letter in enumerate(msgarr):
+        try:
+            entry[id] = tokDict[letter]
+        except:
+            entry[id] = nulTok
+    entry[-1] = eosTok
+    return entry
+
+def dynamicTokenize(
+        lines: list[str],
+        nulTok: int = 0,
+        eosTok: int = 1,
+        lnkTok: int = 2,
+        dType = np.uint32,
+        ) -> npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32]:# if you go any higher fuck off
+    MAX : int = np.iinfo(dType).max
+    link: str
+    entry: npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32] 
+    tokDict : dict[str,int] = {'a':4,'b':5,'c':6,'d':7,'e':8,'f':9,'g':10,'h':11,'i':12,'j':13,'k':14,'l':15,'m':16,'n':17,'o':18,'p':19,'q':20,'r':21,'s':22,'t':23,'u':24,'v':25,'w':26,'x':27,'y':28,'z':29,' ':30,'.':31,',':32,'\'':33,'/':34,'\"':35,':':36,';':37,'1':38,'2':39,'3':40,'4':41,'5':42,'6':43,'7':44,'8':45,'9':46,'0':47}
+    x:list[npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32]]=[]
+    line:str
+    for line in lines:
+        x.extend(__tokenizeLine(line,nulTok,eosTok,lnkTok,dType))
+    y : npt.NDArray[np.uint8] | npt.NDArray[np.uint16] | npt.NDArray[np.uint32] = np.array(x) #ohhhmy god bruh shutUP!!
+    return y
 
 
 
 if __name__ == '__main__':
-    file = "dataset/data.csv"
+    file = "ai/data/data.csv"
     csvfile = open(file, "r")
+    
     readout = list(csv.reader(csvfile))
-
-    charTokenize(readout)
+    goongagas = []
+    for r in readout:
+        goongagas.append(r[3])
+    readout = goongagas
+    x = np.array(dynamicTokenize(readout))
+    print(len(x))
