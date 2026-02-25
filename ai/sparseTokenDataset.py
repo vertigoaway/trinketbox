81b9fb0855f3c733420df29cad877a63ee6ecb00
vertigoaway <connormm2@protonmail.com>
2026-02-25 15:32:06 -0500

    Create a custom text dataloader&set

---
+++ ai/sparseTokenDataset.py
@@ -0,0 +1,30 @@
+import torch
+from torch.utils.data import Dataset
+import numpy.typing as npt
+import numpy as np
+import math as m
+
+import torch.nn.functional as F
+class textDataset(Dataset):
+    def __init__(self, inSize:int, outSize:int, tokenizedData,vocSize:int):
+        ct : int = int(len(tokenizedData)/(inSize+outSize))-1
+        self.ct = ct
+        self.vocSize = vocSize
+        tokenizedData = torch.LongTensor(tokenizedData)
+        inp = []
+        out = []
+        torch.tensor
+        for x in range(ct):
+            inp.append(F.one_hot(tokenizedData[inSize*x:inSize*(x+1)],self.vocSize).to_sparse())
+            out.append(F.one_hot(tokenizedData[inSize*(x+1):(inSize*(x+1))+outSize],self.vocSize).to_sparse())
+        self.inp = inp
+        self.out = out
+        
+        return
+
+    def __len__(self):
+        return self.ct
+
+    def __getitem__(self, idx):
+        #input, expected output
+        return self.inp[idx], self.out[idx]
