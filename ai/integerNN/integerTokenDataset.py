3a186949497978ac62bcd0df8dd0b58586e189f8
vertigoaway <connormm2@protonmail.com>
2026-02-26 16:09:05 -0500

    Use a shifting window, simplify

--- ai/integerNN/integerTokenDataset.py
+++ ai/integerNN/integerTokenDataset.py
@@ -7,22 +7,21 @@ import math as m
 #varient of sparsetokendataset, for integer tokens
 
 class textDataset(Dataset):
-    def __init__(self, inSize:int, outSize:int, tokenizedData, vocSize:int):
+    def __init__(self, inSize:int, outSize:int, tokenizedData, vocSize:int)->None:
         # tokenizedData is expected to be a flat list/array of integer tokens
-        ct = (len(tokenizedData) - outSize) // inSize
-        self.ct = ct
+        self.ct = len(tokenizedData) - outSize+inSize
         self.vocSize = vocSize
-        self.tokenizedData = torch.LongTensor(tokenizedData)
+        self.tokenizedData = torch.LongTensor(tokenizedData) #optimization can be made here
         self.inSize = inSize
         self.outSize = outSize
         
-    def __len__(self):
+    def __len__(self)->int:
         return self.ct
 
-    def __getitem__(self, idx):
-        # return token id sequences directly
-        start = self.inSize * idx
-        inp = self.tokenizedData[start:start+self.inSize]
-        out = self.tokenizedData[start+self.inSize:start+self.inSize+self.outSize]
+    def __getitem__(self, idx : int):#shifting window
+        # return token id sequences
+        endPos : int = idx+self.inSize
+        inp = self.tokenizedData[idx : endPos]
+        out = self.tokenizedData[endPos : endPos + self.outSize]
 
         return inp, out
