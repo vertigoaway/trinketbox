25b22e7a7a5210056ef7b88924a949faf2c2d73c
vertigoaway <connormm2@protonmail.com>
2026-02-27 13:39:18 -0500

    Fix

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 setup(
     name="trinketbox",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
+    install_requires=[
+        "numpy==2.2.6",
+        "setuptools==59.6.0",
+        "torch==2.10.0"
+    ],
 )
