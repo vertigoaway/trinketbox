56e4069fe55d7b83717cf0b57a451518a6cfb140
vertigoaway <connormm2@protonmail.com>
2026-02-28 18:25:06 -0500

    Create charVocab class

--- trinketbox/ai/utils/charTokenizer.py
+++ trinketbox/ai/utils/charTokenizer.py
@@ -6,6 +6,71 @@ from typing import Any
     # 1. add multithreading (OPTIONAL)
 
 
+
+class charVocab():
+    vocabDict : dict[str,int]
+    tokenDict : dict[int,str]
+    nulTok : tuple[int,str]
+    eomTok : tuple[int,str]
+    maxVocSize : int
+    def __init__(self,nulTok=(0,'�'),eomTok=(1,'\n')) -> None:
+        self.nulTok = nulTok
+        self.eomTok = eomTok
+        self.vocabDict = {nulTok[1]:nulTok[0],eomTok[1]:eomTok[0]}
+        self.tokenDict = {nulTok[0]:nulTok[1],eomTok[0]:eomTok[1]}
+        return
+    def __len__(self) -> int:
+        """Get the current length of vocab."""
+        return len(self.vocabDict)
+    def __contains__(self, item : int | str) -> bool:
+        """Check if the specified token/index is set
+        Args:
+            item: The token/index to check"""
+        if type(item) == int:
+            return type(self.tokenDict.get(item))==int
+        elif type(item) == str:
+            return self.vocabDict.get(item)==str
+        else:
+            raise TypeError
+    def __delitem__(self, key : int | str) -> None:
+        """Deletes the specified token/index.
+        Args:
+            key: The token/index to delete."""
+        if type(key) == int:
+            x = self.tokenDict[key]
+            del self.tokenDict[key]
+            del self.vocabDict[x]
+        elif type(key) == str:
+            x = self.vocabDict[key]
+            del self.vocabDict[key]
+            del self.tokenDict[x]
+        else:
+            raise TypeError
+        return
+    def __getitem__(self, key: int | str) -> int | str:
+        if type(key) == int:
+            x = self.tokenDict.get(key)
+            if x == None:
+                return self.nulTok[1]
+            return x
+        elif type(key) == str:
+            x = self.vocabDict.get(key)
+            if x == None:
+                return self.nulTok[0]
+            return x
+        else:
+            raise TypeError
+    def __setitem__(self, key: int | str, value: int | str) -> None:
+        if type(key) == int and type(value) == str:
+            self.tokenDict[key] = value
+            self.vocabDict[value] = key
+        elif type(key) == str and type(value) == int:
+            self.tokenDict[value] = key
+            self.vocabDict[key] = value
+        else:
+            raise TypeError
+        return
+    
 def tokenizeLine( #tokenizes a single line of any size
         msg: str,
         nulTok: int = 0,
@@ -90,7 +155,7 @@ def dynamicDetokenize(
     dType = np.uint32,
     tokDict : dict[str,int] = {'a':4,'b':5,'c':6,'d':7,'e':8,'f':9,'g':10,'h':11,'i':12,'j':13,'k':14,'l':15,'m':16,'n':17,'o':18,'p':19,'q':20,'r':21,'s':22,'t':23,'u':24,'v':25,'w':26,'x':27,'y':28,'z':29,' ':30,'.':31,',':32,'\'':33,'/':34,'\"':35,':':36,';':37,'1':38,'2':39,'3':40,'4':41,'5':42,'6':43,'7':44,'8':45,'9':46,'0':47}
     ) -> list[str]:
-    """Detokenizes a single line of any size, splits on eosTok
+    """Detokenizes an array of any size, splits on eosTok
      Args:
         line: Array of token indices to detokenize.
         nulTok: Replaces all characters unable to be represented.
@@ -99,7 +164,7 @@ def dynamicDetokenize(
         dType: Numpy data type of input array.
         tokDict: Dict mapping chars to token indices.
      Returns:
-        List of detokenized messages, split on eosTok."""
+        List of detokenized strings, split on eosTok."""
     plines:list[list[int]] = [[]]
     ct = 0
     out : list[str] = []
