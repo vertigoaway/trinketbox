2ad97ac4119bf79f65600947ef3060df5d2da8a3
vertigoaway <connormm2@protonmail.com>
2026-03-02 14:16:21 -0500

    Differentiate lazy and regular textDatasets

--- trinketbox/ai/utils/tokenDataset.py
+++ trinketbox/ai/utils/tokenDataset.py
@@ -4,9 +4,33 @@ import numpy.typing as npt
 import numpy as np
 import math as m
 
-#varient of sparsetokendataset, for integer tokens
-
 class textDataset(Dataset):
+    def __init__(self, inSize:int, outSize:int, tokenizedData, vocSize:int,denom=1)->None:
+        # tokenizedData is expected to be a flat list/array of integer tokens
+        device : torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.ct = (len(tokenizedData) - (outSize+inSize+1))//denom
+        self.vocSize = vocSize
+        self.inp = []
+        self.out = []
+        self.inSize = inSize
+        self.outSize = outSize
+        for idx in range(self.ct):
+            endPos : int = idx+self.inSize
+            self.inp.append(torch.LongTensor(tokenizedData[idx : endPos],device=device))
+            self.out.append(torch.LongTensor(tokenizedData[endPos:endPos+self.outSize],device=device))
+
+
+        
+    def __len__(self)->int:
+        return self.ct
+
+    def __getitem__(self, idx : int):#shifting window
+        # return token id sequences
+
+
+        return self.inp[idx], self.out[idx]
+
+class lazyTextDataset(Dataset):
     def __init__(self, inSize:int, outSize:int, tokenizedData, vocSize:int)->None:
         # tokenizedData is expected to be a flat list/array of integer tokens
         self.ct = len(tokenizedData) - (outSize+inSize+1)
