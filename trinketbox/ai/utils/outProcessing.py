99335fd32260d022801db8e9d1c072f6ba054388
vertigoaway <connormm2@protonmail.com>
2026-06-05 20:39:03 -0400

    Add K arg to logitsToId && Patch context handling in interface

--- trinketbox/ai/utils/outProcessing.py
+++ trinketbox/ai/utils/outProcessing.py
@@ -6,16 +6,19 @@ from griot import char
 from griot import word
 def logitsToId(rawLogits:torch.LongTensor | torch.Tensor,
                timeSteps : int,
-               batchSize: int) -> torch.Tensor: 
-    """Converts raw logits to token indices by sampling top 3 tokens per time step.
+               batchSize: int, 
+               k : int = 7
+               ) -> torch.Tensor: 
+    """Converts raw logits to token indices by sampling top K tokens per time step.
     Args:
         rawLogits: (batchSize, timeSteps, vocLen).
         timeSteps: Number of time steps in the output sequence.
-        batchSize: Number of batches."""
+        batchSize: Number of batches.
+        k: Number of tokens to sample"""
     chosenId = np.zeros(shape=(batchSize,timeSteps))
     for batch in range(batchSize):
         for stamp in range(timeSteps):
-            idVals,tokenIds = torch.topk(rawLogits[batch][stamp],k=3,dim=-1)
+            idVals,tokenIds = torch.topk(rawLogits[batch][stamp],k=k,dim=-1)
             idVals = idVals.cpu().detach().numpy()
             tokenIds = tokenIds.cpu().detach().numpy()
             try:
@@ -53,7 +56,7 @@ def inferenceResponse(model,inp: str,
     Returns:
         String containing detokenized response"""
 
-    context : torch.types._TensorOrTensors = torch.LongTensor(voc.tokenizeLine(inp)) # pyright: ignore[reportAttributeAccessIssue]
+    context : torch.types._TensorOrTensors = torch.LongTensor(voc.tokenizeLine(inp)+[voc.eomTok[0]]) # pyright: ignore[reportAttributeAccessIssue]
     a = 0
     out = ''
     while a!=eosTok:
@@ -64,19 +67,18 @@ def inferenceResponse(model,inp: str,
     return out
 
 
-def basicInterface(model, voc: char.Vocab | word.Vocab, memory:str='', timeSteps:int=512,filler:str='�') -> None:
+def basicInterface(model, voc: char.Vocab | word.Vocab, memory:list[str]=[], timeSteps:int=512,filler:str='�') -> None:
     if len(memory)<timeSteps:
-        memory+=filler*(len(memory)-timeSteps)
+        memory += [filler]*(timeSteps-len(memory))
     print(memory)
     cont = True
     while cont:
-        tmp = input('>>').strip()
-        if tmp == 'EXIT':
+        tmp = input('>>').strip().lower()
+        if tmp == 'exit':
             cont = False
             continue
-        tmp = tmp.lower()
-        memory = memory[len(tmp):] + tmp
-        response = inferenceResponse(model,memory,voc)
-        memory = memory[len(response):] + response
+        memory = memory[len(tmp):] + list(tmp)
+        response = inferenceResponse(model,str(memory),voc)
+        memory = memory[len(response):] + list(response)
         print(response)
     return None
